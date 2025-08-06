154c077ed884e22800366db9f9c55d0059dfb0fc
sharlenM <charleneangel.custodio@evsu.edu.ph>
2025-08-06 21:51:17 +0800

    tutorRoutesroutes

--- routes/tutorRoutes.js
+++ routes/tutorRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { requireAuth, requireTutor } from "../middleware/auth.js";
 import { showDashboard } from "../controllers/tutorController.js";
+import upload from "../middleware/upload.js";
+import { addStudentController } from "../controllers/addStudentController.js";
 
 const router = express.Router();
 
+// Dashboard route for tutors
 router.get("/dashboard", requireAuth, requireTutor, showDashboard);
+
+// Add Student Page (GET and POST)
+router.get("/add_student", requireAuth, requireTutor, (req, res) => {
+  res.render("add_student", { user: req.session });
+});
+router.post("/add_student", requireAuth, requireTutor, upload.single('id_picture'), addStudentController);
 
 export default router;
